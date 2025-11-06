c3e139826f53ca32eb1a5511576e1b4c876f54b1
wsk4 <maa.gonzalezva@duocuc.cl>
2025-11-05 21:20:47 -0300

    first commit

---
+++ src/main/java/com/masca/masca_back/MascaBackApplication.java
@@ -0,0 +1,13 @@
+package com.masca.masca_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masca/masca_back/MascaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masca.masca_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
