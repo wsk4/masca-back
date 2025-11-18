5b57e97633e7906ce675f21cf714b7b87e8469fe
wsk4 <maa.gonzalezva@duocuc.cl>
2025-11-18 15:30:40 -0300

    sacar *

--- src/main/java/com/masca/masca_back/controller/EstadoCompraController.java
+++ src/main/java/com/masca/masca_back/controller/EstadoCompraController.java
@@ -1,9 +1,19 @@
 package com.masca.masca_back.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.masca.masca_back.model.EstadoCompra;
 import com.masca.masca_back.service.EstadoCompraService;
 
