e5a6efa6d45b984b2d3b4fe5f281fad904b8e72e
wsk4 <maa.gonzalezva@duocuc.cl>
2025-12-08 14:15:28 -0300

    prueba 1

--- src/main/java/com/masca/masca_back/controller/CategoriaController.java
+++ src/main/java/com/masca/masca_back/controller/CategoriaController.java
@@ -1,7 +1,7 @@
 package com.masca.masca_back.controller;
 
 import java.util.List;
-
+//hola
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
